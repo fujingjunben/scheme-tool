b475773cbaf6b2653275c4d01ef14048229da61c
kuan291 <h.kuan.liu@gmail.com>
2013-12-20 20:19:31 +0800

    sicp eight queens puzzle and clean tool library

--- sicp.scm
+++ sicp.scm
@@ -3,6 +3,9 @@
 ; Newton-method
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (square x)
+  (* x x))
+
 (define (deriv g)
   (lambda (x)
     (/ (- (g (+ x dx)) (g x))
@@ -219,14 +222,6 @@
  (balance? (make-mobile (make-branch 10 1000) 
                          (make-branch 1 level-3-mobile))) 
   
-(define n 1)
-
-(if (> n 0)
-    (begin
-      (newline)
-      (display "nihao")
-      (newline)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 2.30
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -275,6 +270,17 @@
 (define (length-me sequence)
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 2.32
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (cons (car s) x)) rest)))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 2.34
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -300,3 +306,165 @@
               (count-leaves x)
               1))
         t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 2.36
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 2.37
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (x) (dot-product x v)) m))
+
+(define m '((1 2 3 4) (4 5 6 8) (6 7 8 9)))
+(define v '(1 2 3 4))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 2.39
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (reverse-foldr sequence)
+  (fold-right (lambda (x y) (append y (list x))) '() sequence))
+
+(define (reverse-foldl sequence)
+  (fold-left (lambda (x y) (cons y x)) '() sequence))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Nested Mappings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (enumerate-interval start end)
+  (if (> start end)
+      '()
+      (cons start
+            (enumerate-interval (+ start 1) end))))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (permutations s)
+  (if (null? s)
+      (list '())
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 2.40
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (unique-pairs n))))
+
+(define (unique-triple n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (cons j i))
+                  (enumerate-interval 1 (- (cadr i) 1))))
+           (unique-pairs n)))
+
+(define (sum-is? s)
+  (lambda (triple)
+    (= s (accumulate + 0 triple))))
+
+(define (sum-is-s-triples s n)
+  (filter (sum-is? s)
+          (permutations (enumerate-interval 1 n))))
+
+;---------------------------------------
+
+
+(define (make-unique-tuple size max-number)
+  (if (= size 0)
+      (list '())
+      (flatmap (lambda (i)
+                 (map (lambda (t) (cons i t))
+                      (make-unique-tuple (- size 1) (- i 1))))
+               (enumerate-interval 1 max-number))))
+
+  
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 2.42 eight queens puzzle
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define empty-board '())
+
+(define (make-pos col row)
+  (list col row))
+
+(define (select-kth positions)
+  (car positions))
+
+(define (select-rest positions)
+  (cdr positions))
+
+(define (get-col coordinate)
+  (car coordinate))
+
+(define (get-row coordinate)
+  (cadr coordinate))
+
+(define (safe? k positions)
+  (let ((kth (select-kth positions))
+        (rest (select-rest positions)))
+    (let ((kth-row (get-row kth)))
+      (accumulate (lambda (x y) (and x y))
+                  #t
+                  (map (lambda (p)
+                         (let ((rest-col (get-col p))
+                               (rest-row (get-row p)))
+                           (not (or (= kth-row rest-row)
+                                   (= (abs (- k rest-col))
+                                      (abs (- kth-row rest-row)))))))
+                       rest)))))
+
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons (make-pos k new-row) rest-of-queens))
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
---
+++ tool.scm
@@ -0,0 +1,115 @@
+
+(define (divide? b a)
+  (= (remainder b a) 0))
+
+(define (square n)
+  (* n n))
+
+(define (prime? n)
+  (define (traverse lat) 
+    (cond
+     ((null? lat)
+      #t)
+     ((divide? n (car lat))
+      #f)
+     (else
+      (traverse (cdr lat)))))
+  (let-values (((s r) (exact-integer-sqrt n)))
+    (let ((prime-list (soe s)))
+      (traverse prime-list))))
+
+(define (demp n)
+  (define (find-divisor n lat)
+    (cond
+     ((null? lat)
+      (cond
+       ((= n 1) 
+        '())
+       (else
+        (list n))))
+     ((divide? n (car lat))
+      (cons
+       (car lat)
+       (find-divisor (/ n (car lat)) lat)))
+     (else
+      (find-divisor n (cdr lat)))))
+  
+  (let ((prime-list (soe (integer-sqrt n))))
+    (find-divisor n prime-list)))
+
+(define (soe n)
+  (define (compute-soe i j prime-vector)
+    (cond 
+     ((> (square i) n)
+      prime-vector)
+     (else
+      (let ((idx (* i (+ i j))))
+        (cond
+         ((and (vector-ref prime-vector i) (<= idx n))
+          (begin
+            (vector-set! prime-vector idx #f)
+            (compute-soe i (+ j 1) prime-vector)))
+         (else (compute-soe (+ i 1) 0 prime-vector)))))))
+
+  (define (print-soe i prime-list)
+    (cond
+     ((> i n) '())
+     ((car prime-list)
+      (cons i (print-soe (+ i 1) (cdr prime-list))))
+     (else (print-soe (+ i 1) (cdr prime-list)))))
+  
+  (let ((prime-vector (make-vector (+ n 1) #t)))
+    (compute-soe 2 0 prime-vector)
+    (print-soe 2 (cddr (vector->list prime-vector)))))
+
+
+(define (number->list number)
+  (map
+   (lambda (x) (string->number (string x)))
+   (string->list (number->string number))))
+
+(define (str_int->list str)
+  (map 
+   (lambda (x) (string->number (string x)))
+   (string->list str)))
+
+(define (char->int c)
+  (- (char->integer c) 48))
+
+(define (int->list int)
+  (str_int->list (str)))
+
+
+(define (enumerate-interval start end)
+  (if (> start end)
+      '()
+      (cons start
+            (enumerate-interval (+ start 1) end))))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (permutations s)
+  (if (null? s)
+      (list '())
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(define (make-unique-tuple size max-number)
+  (if (= size 0)
+      (list '())
+      (flatmap (lambda (i)
+                 (map (lambda (t) (cons i t))
+                      (make-unique-tuple (- size 1) (- i 1))))
+               (enumerate-interval 1 max-number))))
+
+(define (make-tuple size max-number)
+  (if (= size 0)
+      (list '())
+      (flatmap (lambda (i)
+                 (map (lambda (t) (cons i t))
+                      (make-tuple (- size 1) max-number)))
+               (enumerate-interval 1 max-number))))
+
